62ef45cb9c81f04920e179f24dd55da423f8e268
User <dambert.albano.scc@gmail.com>
2024-03-16 18:37:57 +0800

    monkaW

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 import reactPages from 'vite-plugin-react-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactPages()],
+  plugins: [reactRefresh(), reactPages()],
   base: "/vite-react-router/"
 });
