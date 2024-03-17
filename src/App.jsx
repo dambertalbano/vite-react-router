d4fbe5780c73ba44a579fc2cd3e4ee960d37fde1
User <dambert.albano.scc@gmail.com>
2024-03-17 21:30:14 +0800

    xD

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import AddCategory from "./Components/AddCategory";
 import AddEmployee from "./Components/AddEmployee";
@@ -23,42 +23,30 @@ import TeacherRegister from "./Components/TeacherRegister";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<Start />}></Route>
-        <Route path="/adminlogin" element={<Login />}></Route>
-        <Route path="/employee_login" element={<EmployeeLogin />}></Route>
-        <Route path="/employee_detail/:id" element={<EmployeeDetail />}></Route>
-        <Route path="/teacher_login" element={<TeacherLogin />}></Route>
-        <Route path="/teacher_detail/:id" element={<TeacherDetail />}></Route>
-        <Route path="/teacher_register" element={<TeacherRegister />}></Route>
-        <Route path="/employee_register" element={<EmployeeRegister />}></Route>
+        <Route path="/" element={<Start />} />
+        <Route path="/adminlogin" element={<Login />} />
+        <Route path="/employee_login" element={<EmployeeLogin />} />
+        <Route path="/employee_detail/:id" element={<EmployeeDetail />} />
+        <Route path="/teacher_login" element={<TeacherLogin />} />
+        <Route path="/teacher_detail/:id" element={<TeacherDetail />} />
+        <Route path="/teacher_register" element={<TeacherRegister />} />
+        <Route path="/employee_register" element={<EmployeeRegister />} />
         <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="" element={<Home />}></Route>
-          <Route path="/dashboard/employee" element={<Employee />}></Route>
-          <Route path="/dashboard/teacher" element={<Teacher />}></Route>
-          <Route path="/dashboard/category" element={<Category />}></Route>
-          <Route path="/dashboard/profile" element={<Profile />}></Route>
-          <Route
-            path="/dashboard/add_category"
-            element={<AddCategory />}
-          ></Route>
-          <Route
-            path="/dashboard/add_employee"
-            element={<AddEmployee />}
-          ></Route>
-          <Route
-            path="/dashboard/edit_employee/:id"
-            element={<EditEmployee />}
-          ></Route>
-          <Route path="/dashboard/add_teacher" element={<AddTeacher />}></Route>
-          <Route
-            path="/dashboard/edit_teacher/:id"
-            element={<EditTeacher />}
-          ></Route>
+          <Route index element={<Home />} />
+          <Route path="employee" element={<Employee />} />
+          <Route path="teacher" element={<Teacher />} />
+          <Route path="category" element={<Category />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="add_category" element={<AddCategory />} />
+          <Route path="add_employee" element={<AddEmployee />} />
+          <Route path="edit_employee/:id" element={<EditEmployee />} />
+          <Route path="add_teacher" element={<AddTeacher />} />
+          <Route path="edit_teacher/:id" element={<EditTeacher />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
